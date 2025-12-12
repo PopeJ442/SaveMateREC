8e3bca20e8ac5b8e05f73c345a777ffbe110b17a
Popej442 <minnejefferson3@gmail.com>
2025-12-12 21:34:29 +0000

    all done

--- Savemate.Application/obj/Debug/net8.0/Savemate.Application.AssemblyInfo.cs
+++ Savemate.Application/obj/Debug/net8.0/Savemate.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Savemate.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f65548e585976cd1078aba1ff38523d040ce29c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9600d10d9e218138d6cbe59eda0947a9bf616128")]
 [assembly: System.Reflection.AssemblyProductAttribute("Savemate.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Savemate.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
